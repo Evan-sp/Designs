5f23f470964aca0b52f6f1e34ad88c0a9362faf0
Evan-sp <epspensley@gmail.com>
2025-01-04 17:14:13 -0500

    chaos

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Adjust the path to your project structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
